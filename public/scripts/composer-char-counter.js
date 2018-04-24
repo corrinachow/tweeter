154f649f7e5880b05da794a3716946a4b0afcbb1
corrinachow <corrina.yy.chow@gmail.com>
2018-04-24 17:47:27 -0400

    Add tweets style

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,11 @@
-$(document).ready(function() {
+$(function() {
   $(".new-tweet")
     .find("form")
     .on("keyup", "textarea", function() {
-      let counter = $(".counter");
+      let counter = $(this).siblings('.counter');
       let length = $(this).val().length;
       counter.text(140 - length);
-      length <= 140
-        ? counter.css("color", "black")
-        : counter.css("color", "red");
+      length <= 140 ? counter.css("color", "black") : counter.css("color", "red");
     });
 });
+
