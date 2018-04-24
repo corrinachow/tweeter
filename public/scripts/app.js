7f8d984fa8380fa5208289fc8493f57abdd9602d
corrinachow <corrina.yy.chow@gmail.com>
2018-04-24 19:23:46 -0400

    Add dynamic Tweet rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,111 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: {
+        small: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        regular: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        large: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      handle: "@SirIsaac"
+    },
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants"
+    },
+    created_at: 1461116232227
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: {
+        small: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        regular: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        large: "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      handle: "@rd"
+    },
+    content: {
+      text: "Je pense , donc je suis"
+    },
+    created_at: 1461113959088
+  },
+  {
+    user: {
+      name: "Johann von Goethe",
+      avatars: {
+        small: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        regular: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        large: "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      handle: "@johann49"
+    },
+    content: {
+      text: "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    created_at: 1461113796368
+  }
+];
 
+const dateIntervals = {
+  years: { intervalName: "year", seconds: 31557600 },
+  month: { intervalName: "month", seconds: 2592000 },
+  days: { intervalName: "days", seconds: 86400 },
+  hour: { intervalName: "hour", seconds: 3600 }
+};
+
+function timeSince(date) {
+  // Gets seconds difference between now and post date
+  const secondsDifference = Math.floor((new Date() - date) / 1000);
+
+  for (const interval in dateIntervals) {
+    const timeInterval = Math.floor(
+      secondsDifference / dateIntervals[interval].seconds
+    );
+    if (timeInterval > 0) {
+      return `${timeInterval} ${dateIntervals[interval].intervalName} ago`;
+    }
+  }
+  return `${Math.floor(secondsDifference)} seconds ago`;
+}
+
+function createTweetElement(tweet) {
+  const { name, avatars, handle } = tweet.user;
+  const { text } = tweet.content;
+
+  const $tweet = $("<article>").addClass("tweet");
+
+  // Tweet header
+  const avatar = $("<img>").attr("src", avatars.regular);
+  const $h2 = $("<h2>").text(name);
+  const $handle = $("<span>")
+    .addClass("handle")
+    .text(handle);
+  const $header = $("<header>").append(avatar, $("<div>").append($h2, $handle));
+
+  // Tweet body
+  const $tweetBody = $("<p>").text(text);
+
+  // Tweet footer
+  const $date = $("<span>").text(timeSince(tweet.created_at));
+  const $tweetActions = `<div class='tweet-actions'>
+  <span class='report'><i class="fab fa-font-awesome-flag"></i></span>
+      <span class='retweet'><i class="fas fa-retweet"></i></span>
+      <span class='like'><i class="fas fa-heart"></i></span>
+    </div>`;
+  const $footer = $("<footer>").append($date, $tweetActions);
+
+  return $tweet.append($header, $tweetBody, $footer);
+}
+
+function renderTweets(tweets) {
+  let tweetList = [];
+  for (let tweet of tweets) {
+    tweetList.push(createTweetElement(tweet));
+  }
+  return tweetList;
+}
+
+$(function() {
+  $("#tweets-container").append(renderTweets(data));
+});
--- public/scripts/tweets.js
+++ public/scripts/tweets.js
@@ -1,36 +1,18 @@
-const dateIntervals = {
-  years: { intervalName: "year", ms: 31536000 },
-  month: { intervalName: "month", ms: 2592000 },
-  days: { intervalName: "days", ms: 86400 },
-  minutes: { intervalName: "minutes", ms: 3600 }
-};
-
-function timeSince(date) {
-  // Gets seconds difference between now and post date
-  const secondsDifference = Math.floor((new Date() - date) / 1000);
-  for (const interval in dateIntervals) {
-    const timeInterval = Math.floor(
-      secondsDifference / dateIntervals[interval].ms
-    );
-    if (timeInterval > 1) {
-      return `${timeInterval} ${dateIntervals[interval].intervalName}`;
-    }
-  }
-  return `${Math.floor(secondsDifference)} seconds`;
-}
-
 $(function() {
-  const $tweetActions = $(".tweet-actions");
-  const $header = $(".tweet").find("header");
-  const $tweetTime = $("article.tweet .date").text();
 
-  $("article.tweet").mouseenter(e => {
+  // const $header = $(".tweet").find("header");
+  // const $tweetTime = $("article.tweet .date").text();
+
+  $("article.tweet").mouseenter(function() {
+    const $tweetActions = $(this).find('.tweet-actions');
+    const $header = $(this).find('header');
     $tweetActions.css("visibility", "visible");
     $header.css("opacity", "1");
   });
-  $("article.tweet").mouseleave(e => {
+  $("article.tweet").mouseleave(function() {
+    const $tweetActions = $(this).find('.tweet-actions');
+    const $header = $(this).find('header');
     $tweetActions.css("visibility", "hidden");
     $header.css("opacity", "0.8");
   });
-  $(".date").text(timeSince($tweetTime));
 });
